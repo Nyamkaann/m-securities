1486e5d2ec0042733f3e769b4995a3395b5d59da
Nyamdorj <nyamdorjmunkh@gmail.com>
2025-02-27 15:56:24 +0800

    keywords added

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 const defaultMetadata: Metadata = {
   title: 'M Securities',
   description: 'M Securities is a leading financial services company in Mongolia providing secure, reliable, and flexible investment solutions.',
-  keywords: 'M Securities, investment, trading, broker, Mongolia, stock market, financial services',
+  keywords: 'M Securities, investment, trading, broker, Mongolia, stock market, financial services, M bank, Mongolian Mining brokerage, Mining, Underwritter, Mongolian bond, Mcs brokerage, Mcs, Securities, M',
   authors: [{ name: 'M Securities' }],
   creator: 'M Securities',
   publisher: 'M Securities',
